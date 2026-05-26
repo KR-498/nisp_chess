1dd5f954a0bfd2b41a9b64c7a6480b38dc50515f
Kacper Radon <40498@student.atar.edu.pl>
2026-05-26 08:37:44 +0200

    feat: dodano ruch krola

--- nisp_chess/King.js
+++ nisp_chess/King.js
@@ -5,8 +5,20 @@ class King {
     }
 
     validateMove(move) {
-        
-        // dokoncz implementacje
+        // Obliczamy dystans przesunięcia w osi X oraz Y
+        const distanceX = Math.abs(move.destinationX - move.sourceX);
+        const distanceY = Math.abs(move.destinationY - move.sourceY);
+
+        // Król nie może stanąć w miejscu (ruch musi się odbyć)
+        if (distanceX === 0 && distanceY === 0) {
+            return false;
+        }
+
+        // Ruch jest poprawny, jeśli król przesuwa się o maksymalnie 1 pole w każdą stronę
+        if (distanceX <= 1 && distanceY <= 1) {
+            return true;
+        }
+
         return false;
     }
 }

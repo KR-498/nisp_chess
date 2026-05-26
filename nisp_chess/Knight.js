d36e6eed4d20c088e69553abe1b903f5f8bd514e
Kacper Radon <40498@student.atar.edu.pl>
2026-05-26 14:33:02 +0200

    feat: dodanie ruchu konika i usuniecie zbednych plikow

--- nisp_chess/Knight.js
+++ nisp_chess/Knight.js
@@ -1,12 +1,19 @@
-// Klasa reprezentująca skoczka / konika
+// Klasa reprezentująca skoczka (konika)
 class Knight {
     constructor() {
         this.type = "KNIGHT";
     }
 
     validateMove(move) {
-        
-        // dokoncz implementacje
-        return false;
+        // Obliczamy bezwzględną odległość w pionie i poziomie
+        const diffX = Math.abs(move.destinationX - move.sourceX);
+        const diffY = Math.abs(move.destinationY - move.sourceY);
+
+        // Ruch jest poprawny, gdy różnice pól to (1 i 2) lub (2 i 1)
+        if ((diffX === 1 && diffY === 2) || (diffX === 2 && diffY === 1)) {
+            return true; // Poprawny ruch skoczka
+        } else {
+            return false; // Niepoprawny ruch dla skoczka
+        }
     }
 }

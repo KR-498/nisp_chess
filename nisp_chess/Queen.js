f62dfd8f961ba2c97cf3b350c1d0d9b471fdd7cc
Ernest Pacocha <epacocha4@gmail.com>
2026-05-26 09:06:15 +0200

    feat: dodano dzialanie krolowej

--- nisp_chess/Queen.js
+++ nisp_chess/Queen.js
@@ -1,12 +1,24 @@
-// Klasa reprezentująca królową
 class Queen {
     constructor() {
         this.type = "QUEEN";
     }
 
     validateMove(move) {
-        
-        // dokoncz implementacje
-        return false;
+        const dX = Math.abs(move.to.x - move.from.x);
+        const dY = Math.abs(move.to.y - move.from.y);
+
+        // Ruch w miejscu jest nieprawidłowy
+        if (dX === 0 && dY === 0) {
+            return false;
+        }
+
+        // Ruch jak wieża (tylko w pionie lub tylko w poziomie)
+        const isRookMove = dX === 0 || dY === 0;
+
+        // Ruch jak goniec (po skosie - przesunięcie X i Y jest równe)
+        const isBishopMove = dX === dY;
+
+        // Królowa łączy oba te ruchy
+        return isRookMove || isBishopMove;
     }
 }
